2f22a8b8636ff5208aafc03919233a566195edfd
Dan Peach <daniel.peach@armory.io>
2021-04-30 12:16:31 -0400

    refactor, allow builds in repos that aren't the source repo

--- src/main.ts
+++ src/main.ts
@@ -1,84 +1,31 @@
 import * as core from '@actions/core'
 import {context} from '@actions/github'
 import {exec} from '@actions/exec'
+import {Inputs, generateVariables} from './variables'
 
 async function run(): Promise<void> {
-  const artifactoryOrg = core.getInput('artifactory_org')
-  const artifactoryDockerRepository = core.getInput(
-    'artifactory_docker_repository'
-  )
-  const dockerRepositoryPrefix = core.getInput('docker_repository_prefix')
-  const redHatScanRegistryHostname = core.getInput(
-    'red_hat_scan_registry_hostname'
-  )
-  const redHatPid = core.getInput('red_hat_pid') ?? ''
-  const {runId, ref} = context
-
-  const sanitizedRef = getSanitizedRef(ref)
-
-  const artifactoryDockerRegistryHostname = `${artifactoryOrg}-${artifactoryDockerRepository}.jfrog.io`
-
-  core.setOutput('org', context.repo.owner)
-  core.setOutput('repo', context.repo.repo)
-  core.setOutput('build_number', `${context.sha}:${context.runId}`)
-  core.setOutput('build_name', `${context.repo.owner}:${context.repo.repo}`)
-  core.setOutput(
-    'build_url',
-    `https://github.com/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}`
-  )
-  core.setOutput(
-    'artifactory_docker_registry_hostname',
-    artifactoryDockerRegistryHostname
-  )
-  core.setOutput(
-    'artifactory_url',
-    `https://${artifactoryOrg}.jfrog.io/artifactory`
-  )
-  core.setOutput('artifactory_docker_repository', artifactoryDockerRepository)
-
-  core.setOutput('red_hat_scan_registry_hostname', redHatScanRegistryHostname)
-
-  // There are some cases where the version is supplied by Astrolabe, rather than inferring from
-  // the git repo.
-  const commitDate = await (async () => {
-    try {
-      return await getCommitDate()
-    } catch {
-      return null
-    }
-  })()
-
-  if (!commitDate) {
-    return
+  const inputs: Inputs = {
+    artifactoryOrg: core.getInput('artifactory_org'),
+    artifactoryDockerRepository: core.getInput('artifactory_docker_repository'),
+    dockerRepositoryPrefix: core.getInput('docker_repository_prefix'),
+    redHatScanRegistryHostname: core.getInput('red_hat_scan_registry_hostname'),
+    redHatPid: core.getInput('red_hat_pid') ?? '',
+    runId: context.runId,
+    ref: context.ref,
+    org: core.getInput('org') ?? context.repo.owner,
+    repo: core.getInput('repo') ?? context.repo.repo,
+    buildOrg: context.repo.owner,
+    buildRepo: context.repo.repo
   }
 
-  const version = `${commitDate}.${sanitizedRef}`
-  const imageName = `${dockerRepositoryPrefix}/${context.repo.repo}:${version}`
-  core.setOutput('version', version)
-  core.setOutput('image_name', imageName)
-  core.setOutput(
-    'artifactory_image_name',
-    `${artifactoryDockerRegistryHostname}/${imageName}`
-  )
-  core.setOutput('ubi_image_name', `${imageName}-ubi`)
-  core.setOutput(
-    'ubi_scan_image_name',
-    `${redHatScanRegistryHostname}/${redHatPid}/${context.repo.repo}:${version}-ubi`
-  )
-}
+  const outputs = await generateVariables(inputs, {
+    resolve: getCommitDate
+  })
 
-// This is sanitized for Docker image tags.
-const getSanitizedRef = (ref: string): string => {
-  if (ref.startsWith('refs/heads/')) {
-    return sanitize(ref.substring('refs/heads/'.length))
-  } else if (ref.startsWith('refs/tags/')) {
-    return sanitize(ref.substring('refs/tags/'.length))
-  } else {
-    throw new Error(`Invalid git ref: ${ref}`)
-  }
+  Object.entries(outputs).forEach(([key, value]) => {
+    core.setOutput(key, value)
+  })
 }
-
-const sanitize = (str: string) => str.replace(/[^0-9a-z]/gi, '')
 
 const getCommitDate = async (): Promise<string> => {
   let commitDateIso = ''
@@ -89,18 +36,7 @@ const getCommitDate = async (): Promise<string> => {
       }
     }
   })
-  const commitDate = new Date(commitDateIso.trim())
-  return `${commitDate.getUTCFullYear()}.${
-    (commitDate.getUTCMonth() + 1).toString().padStart(2, '0')
-  }.${
-    commitDate.getUTCDate().toString().padStart(2, '0')
-  }.${
-    commitDate.getUTCHours().toString().padStart(2, '0')
-  }.${
-    commitDate.getUTCMinutes().toString().padStart(2, '0')
-  }.${
-    commitDate.getUTCSeconds().toString().padStart(2, '0')
-  }`
+  return commitDateIso.trim()
 }
 
 run().catch(e => core.setFailed(e))
---
+++ src/variables.ts
@@ -0,0 +1,111 @@
+import * as core from '@actions/core'
+import {context} from '@actions/github'
+
+export interface Inputs {
+  artifactoryOrg: string
+  artifactoryDockerRepository: string
+  dockerRepositoryPrefix: string
+  redHatScanRegistryHostname: string
+  redHatPid: string
+
+  runId: number
+  ref: string
+
+  org: string
+  repo: string
+
+  // In some cases, the build is happening in a different repo from
+  // the code source repo.
+  // For example, if we are building spinnaker/deck
+  // inside of armory-io/oss-artifact-builder.
+  buildOrg: string
+  buildRepo: string
+}
+
+export interface TimestampResolver {
+  // Retuns build timestamp in ISO 8601.
+  resolve(): Promise<string>
+}
+
+export const generateVariables = async (
+  inputs: Inputs,
+  resolver: TimestampResolver
+): Promise<{ [key: string]: string }> => {
+  const {
+    artifactoryOrg,
+    artifactoryDockerRepository,
+    dockerRepositoryPrefix,
+    redHatScanRegistryHostname,
+    redHatPid,
+    runId,
+    ref
+  } = inputs
+
+  const outputs: {[key: string]: string} = {}
+
+  const artifactoryDockerRegistryHostname = `${artifactoryOrg}-${artifactoryDockerRepository}.jfrog.io`
+
+  outputs['org'] = context.repo.owner
+  outputs['repo'] = context.repo.repo
+  outputs['build_number'] = `${context.sha}:${context.runId}`
+  outputs['build_name'] = `${context.repo.owner}:${context.repo.repo}`
+  outputs[ 'build_url'] = `https://github.com/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}`
+  outputs['artifactory_docker_registry_hostname'] = artifactoryDockerRegistryHostname
+  outputs['artifactory_url'] = `https://${artifactoryOrg}.jfrog.io/artifactory`
+  outputs['artifactory_docker_repository'] = artifactoryDockerRepository
+  outputs['red_hat_scan_registry_hostname'] = redHatScanRegistryHostname
+
+  // There are some cases where the version is supplied by Astrolabe, rather than inferring from
+  // the git repo.
+  let versionTimestamp;
+  try {
+    versionTimestamp = convertIso(await resolver.resolve())
+  } catch {
+    return outputs
+  }
+
+  const sanitizedRef = getSanitizedRef(ref)
+  const version = `${versionTimestamp}.${sanitizedRef}`
+  const imageName = `${dockerRepositoryPrefix}/${context.repo.repo}:${version}`
+
+  outputs['version'] = version
+  outputs['image_name'] = imageName
+  outputs['artifactory_image_name'] = `${artifactoryDockerRegistryHostname}/${imageName}`
+  outputs['ubi_image_name'] = `${imageName}-ubi`
+  outputs['ubi_scan_image_name'] = `${redHatScanRegistryHostname}/${redHatPid}/${context.repo.repo}:${version}-ubi`
+
+  return outputs
+}
+
+// This is sanitized for Docker image tags.
+const getSanitizedRef = (ref: string): string => {
+  if (ref.startsWith('refs/heads/')) {
+    return sanitize(ref.substring('refs/heads/'.length))
+  } else if (ref.startsWith('refs/tags/')) {
+    return sanitize(ref.substring('refs/tags/'.length))
+  } else {
+    throw new Error(`Invalid git ref: ${ref}`)
+  }
+}
+
+const sanitize = (str: string) => str.replace(/[^0-9a-z]/gi, '')
+
+const convertIso = (iso: string): string =>  {
+  const date = new Date(iso)
+
+  return `${date.getUTCFullYear()}.${(date.getUTCMonth() + 1)
+    .toString()
+    .padStart(2, '0')}.${date
+    .getUTCDate()
+    .toString()
+    .padStart(2, '0')}.${date
+    .getUTCHours()
+    .toString()
+    .padStart(2, '0')}.${date
+    .getUTCMinutes()
+    .toString()
+    .padStart(2, '0')}.${date
+    .getUTCSeconds()
+    .toString()
+    .padStart(2, '0')}`
+}
